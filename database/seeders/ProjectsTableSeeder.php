5dbeb723af6b34c190765f03922f18749929eb10
Irshandy Juniar Hardadi <48887336+irsjrhr@users.noreply.github.com>
2026-08-06 09:38:47 +0700

    Empty seeder data

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -18,23 +18,7 @@ class ProjectsTableSeeder extends Seeder
 
         \DB::table('projects')->delete();
         
-        \DB::table('projects')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
-                'name' => 'DMS Distributor Management System',
-                'key' => 'PRJ-ARX/2026/07/00001',
-                'description' => NULL,
-                'owner_id' => 1,
-                'status' => 'Active',
-                'workflow_config' => NULL,
-                'custom_field_definitions' => NULL,
-                'created_at' => '2026-07-09 08:20:13',
-                'updated_at' => '2026-07-09 09:27:31',
-                'start_date' => NULL,
-                'end_date' => NULL,
-            ),
-        ));
+        \DB::table('projects')->insert(array ());
         
         
     }
